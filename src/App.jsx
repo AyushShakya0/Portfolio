918c23bbe2b647d717e0589c04b6098dad0e2f70
Ayush Shakya <shakyaayush82@gmail.com>
2026-04-08 14:42:15 +0545

    Implementation of the Safari page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import gsap from 'gsap';
 import { Draggable, ScrollTrigger,SplitText } from 'gsap/all'
 
 import { Navbar, Welcome, Dock } from '#components'
-import { Terminal } from '#windows';
+import { Terminal, Safari } from '#windows';
 
 gsap.registerPlugin(Draggable,ScrollTrigger,SplitText);
 
@@ -17,6 +17,7 @@ const App = () => {
         <Dock/>
 
         <Terminal/>
+        <Safari/>
     </main>
   )
 }
--- src/components/WindowControls.jsx
+++ src/components/WindowControls.jsx
@@ -10,6 +10,14 @@ const WindowControls = ({target}) => {
         <div className='maximize' />
         {/* need to implement maximize and minimize */}
         {/* and later dark mode too */}
+
+
+{/* ideas not sure if it will work */}
+        {/* to maximize dont use window wrapper and to minimize just remove the opacity ig  */}
+        {/* and when i reopen after minimizing use fromto to scale it up and bring back the opacity */}
+        {/* but will have to manage state of whether it was maximized before or not to show that
+         but to properly scale it down after maximizing will be a challenge maybe*/}
+         {/* so just close and reopen it to reset maybe???? */}
       
     </div>
   )
